68049c199eb58d006fef2607825f1f9767c90249
d4niell <67240995+d4niell@users.noreply.github.com>
2021-07-15 13:23:48 +0100

    Add files via upload

--- Enhanced Console.cpp
+++ Enhanced Console.cpp
@@ -5,12 +5,15 @@
 #include "Enhanced Console.h"
 #include <Windows.h>
 
+
+#define MODE_MENU_DEBUG_MODE 4
 #define MODE_MENU_DEVELOPER_MODE 1
 #define MODE_MENU_DEFAULT_MODE 3
-#define OPTIONS_MENU 2
+#define OPTIONS_MENU_EXIT 2
+#define OPTIONS_MENU_APPAREANCE 5
 #define MAX_LOADSTRING 100
 void AddMenus(HWND);
-
+void AddControls(HWND);
 HMENU hMenu;
 // Global Variables:
 HINSTANCE hInst;                                // current instance
@@ -135,6 +138,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     case WM_CREATE:
         {
         AddMenus(hWnd);
+        AddControls(hWnd);
         break;
         }
     case WM_COMMAND:
@@ -143,20 +147,32 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             // Parse the menu selections:
             switch (wmId)
             {
+            case OPTIONS_MENU_APPAREANCE:
+            {
+            
+            
+            }
+            case MODE_MENU_DEBUG_MODE:
+            {
+                MessageBeep(MB_ICONINFORMATION);
+                MessageBox(hWnd, L"Do you really want to enable Debug mode?", L"Mode Selection ...", MB_ICONQUESTION);
+                break;
+            
+            }
             case 3:
             {
                 MessageBeep(MB_OK);
                 MessageBox(hWnd, L"Mode Has Been Changed Back To Default Mode!", L"Mode Selection ...", MB_OK);
                 break;
             }
-            case 2:
+            case OPTIONS_MENU_EXIT:
             {
 
                 MessageBeep(MB_ICONWARNING);
                 DestroyWindow(hWnd);
             
             }
-            case 1:
+            case MODE_MENU_DEVELOPER_MODE:
             {
               
                 MessageBeep(MB_ICONWARNING);
@@ -199,17 +215,27 @@ void AddMenus(HWND hWnd)
     hMenu = CreateMenu();
     HMENU hModeMenu = CreateMenu();
     HMENU hOptionsMenu = CreateMenu();
+    HMENU hSubOptionsMenu = CreateMenu();
+
+
+    //Appareance Sub Options Menu start
+    AppendMenu(hSubOptionsMenu, MF_STRING, OPTIONS_MENU_APPAREANCE, L"Red");
+    AppendMenu(hSubOptionsMenu, MF_STRING, OPTIONS_MENU_APPAREANCE, L"Green");
+    AppendMenu(hSubOptionsMenu, MF_STRING, OPTIONS_MENU_APPAREANCE, L"Blue");
+    //Appareance Sub Options Menu end
 
     //Mode (experimental) functions start
     AppendMenu(hModeMenu, MF_STRING, MODE_MENU_DEVELOPER_MODE, L"Developer Mode");
     AppendMenu(hModeMenu, MF_STRING, MODE_MENU_DEFAULT_MODE, L"Default Mode");
     AppendMenu(hModeMenu, MF_SEPARATOR, NULL, NULL);
+    AppendMenu(hModeMenu, MF_STRING, MODE_MENU_DEBUG_MODE, L"Debug");
     //Mode (experimental) functions end
 
 
 
     //Options functions start
-    AppendMenu(hOptionsMenu, MF_STRING, OPTIONS_MENU, L"Exit Enhanced Console");
+    AppendMenu(hOptionsMenu, MF_STRING, OPTIONS_MENU_EXIT, L"Exit Enhanced Console");
+    AppendMenu(hOptionsMenu, MF_POPUP, (UINT_PTR) hSubOptionsMenu, L"Appareance");
     //Options function end
 
 
@@ -219,6 +245,13 @@ void AddMenus(HWND hWnd)
 
 
 }
+void AddControls(HWND hWnd)
+{
+    CreateWindowW(L"Static", L"Enhanced Console Debug", WS_VISIBLE |WS_TILEDWINDOW, 150, 150, 100, 100, hWnd, NULL, NULL, NULL);
+    CreateWindowW(L"Static", L"->", WS_VISIBLE | WS_CHILDWINDOW, 150, 150, 250, 250, hWnd, NULL, NULL, NULL);
+
+}
+
 // Message handler for about box.
 INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 {
